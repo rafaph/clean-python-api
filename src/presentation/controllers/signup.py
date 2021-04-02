f32652e0261441afc2eda530fee2d8f058cf068a
Raphael Castro <rafaphcastro@gmail.com>
2021-04-02 00:58:02 -0300

    feat: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.py
+++ src/presentation/controllers/signup.py
@@ -5,7 +5,7 @@ from src.presentation.helpers import bad_request
 
 class SignUpController:
     def handle(self, httpRequest: HttpRequest) -> HttpResponse:
-        required_fields = ["name", "email"]
+        required_fields = ["name", "email", "password"]
 
         for field in required_fields:
             if field not in httpRequest["body"]:
