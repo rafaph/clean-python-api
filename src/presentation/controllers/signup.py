3a18d1538582c564a42917c7b30df2901aa43d75
Raphael Castro <rafaphcastro@gmail.com>
2021-04-02 00:06:06 -0300

    feat: ensure SignUpController returns a error if no name is provided

--- src/presentation/controllers/signup.py
+++ src/presentation/controllers/signup.py
@@ -1,6 +1,16 @@
 from typing import Any
 
 
+class ValidationError(Exception):
+    message = None
+
+    def __init__(self, message):
+        self.message = message
+
+    def __eq__(self, instance):
+        return instance.message == self.message
+
+
 class SignUpController:
     def handle(self, httpRequest: Any) -> Any:
-        return {"statusCode": 400}
+        return {"statusCode": 400, "body": ValidationError("Missing param: name")}
