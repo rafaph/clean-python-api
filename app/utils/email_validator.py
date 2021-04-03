13ab1cf19551525cc3185a2828fa8b045c9905b6
Raphael Castro <rafaphcastro@gmail.com>
2021-04-03 16:22:10 -0300

    feat: ensure EmailValidatorAdapter returns if validator returns false

---
+++ app/utils/email_validator.py
@@ -0,0 +1,6 @@
+from app.presentation.protocols.email_validator import EmailValidator
+
+
+class EmailValidatorAdapter(EmailValidator):
+    def is_valid(self, email: str) -> bool:
+        return False
