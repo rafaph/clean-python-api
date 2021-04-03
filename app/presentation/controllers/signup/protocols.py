1058ceab84f744060a80dfd87fe4ca5104c6a463
Raphael Castro <rafaphcastro@gmail.com>
2021-04-03 18:10:44 -0300

    refactor: adjust signup protocols to absolute imports

--- app/presentation/controllers/signup/protocols.py
+++ app/presentation/controllers/signup/protocols.py
@@ -1,7 +1,7 @@
-from ....domain.models.account import AccountModel
-from ....domain.usecases.add_account import AddAccount, AddAccountModel
-from ...protocols import Controller, HttpRequest, HttpResponse
-from ...protocols.email_validator import EmailValidator
+from app.domain.models.account import AccountModel
+from app.domain.usecases.add_account import AddAccount, AddAccountModel
+from app.presentation.protocols import Controller, HttpRequest, HttpResponse
+from app.presentation.protocols.email_validator import EmailValidator
 
 __all__ = [
     "HttpRequest",
