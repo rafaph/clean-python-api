add2278fa23df15850a5513d9c77ed1c6ebbbeeb
Raphael Castro <rafaphcastro@gmail.com>
2021-04-03 18:51:01 -0300

    test: ensure DbAddAccount calls AddAccountRepository with correct values

--- app/data/usecases/add_account/test_db_add_account.py
+++ app/data/usecases/add_account/test_db_add_account.py
@@ -2,7 +2,12 @@ from dataclasses import dataclass
 from unittest import TestCase, mock
 
 from .db_add_account import DbAddAccount
-from .db_add_account_protocols import Encrypter, AddAccountModel
+from .db_add_account_protocols import (
+    Encrypter,
+    AddAccountModel,
+    AccountModel,
+    AddAccountRepository,
+)
 
 
 def make_encrypter() -> Encrypter:
@@ -13,17 +18,37 @@ def make_encrypter() -> Encrypter:
     return EncrypterStub()
 
 
+def make_add_account_repository() -> AddAccountRepository:
+    class AddAccountRepositoryStub(AddAccountRepository):
+        def add(self, account_data: AddAccountModel) -> AccountModel:
+            fake_account = AccountModel(
+                id="valid_id",
+                name="valid_name",
+                email="valid_email@mail.com",
+                password="hashed_password",
+            )
+            return fake_account
+
+    return AddAccountRepositoryStub()
+
+
 @dataclass
 class SutTypes:
     encrypter_stub: Encrypter
+    add_account_repository_stub: AddAccountRepository
     sut: DbAddAccount
 
 
 def make_sut():
     encrypter_stub = make_encrypter()
-    sut = DbAddAccount(encrypter_stub)
+    add_account_repository_stub = make_add_account_repository()
+    sut = DbAddAccount(encrypter_stub, add_account_repository_stub)
 
-    return SutTypes(sut=sut, encrypter_stub=encrypter_stub)
+    return SutTypes(
+        sut=sut,
+        encrypter_stub=encrypter_stub,
+        add_account_repository_stub=add_account_repository_stub,
+    )
 
 
 class DbAddAccountTests(TestCase):
@@ -44,7 +69,9 @@ class DbAddAccountTests(TestCase):
             name="valid_name", email="valid_email@mail.com", password="valid_password"
         )
 
-        with mock.patch.object(encrypter_stub, "encrypt") as encrypt_spy:
+        with mock.patch.object(
+            encrypter_stub, "encrypt", return_value="hashed_password"
+        ) as encrypt_spy:
             sut.add(account_data)
             encrypt_spy.assert_called_once_with("valid_password")
 
@@ -67,3 +94,29 @@ class DbAddAccountTests(TestCase):
             encrypter_stub, "encrypt", encrypt_mock
         ), self.assertRaises(Exception):
             sut.add(account_data)
+
+    @staticmethod
+    def test_call_add_account_repository_with_correct_values():
+        """
+        Should call AddAccountRepository with correct values
+        """
+
+        sut_types = make_sut()
+        sut, add_account_repository_stub = (
+            sut_types.sut,
+            sut_types.add_account_repository_stub,
+        )
+
+        account_data = AddAccountModel(
+            name="valid_name", email="valid_email@mail.com", password="valid_password"
+        )
+
+        with mock.patch.object(add_account_repository_stub, "add") as add_spy:
+            sut.add(account_data)
+            add_spy.assert_called_once_with(
+                AddAccountModel(
+                    name="valid_name",
+                    email="valid_email@mail.com",
+                    password="hashed_password",
+                )
+            )
