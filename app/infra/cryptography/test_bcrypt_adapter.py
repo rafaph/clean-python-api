82ba7f5256d51ea9f7311a308037597190e85496
Raphael Castro <rafaphcastro@gmail.com>
2021-04-03 23:30:02 -0300

    test: ensure BcryptAdapter returns a hash on success

--- app/infra/cryptography/test_bcrypt_adapter.py
+++ app/infra/cryptography/test_bcrypt_adapter.py
@@ -13,30 +13,33 @@ class BcryptAdapterTests(TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.hashpw_patcher = mock.patch("app.infra.cryptography.bcrypt_adapter.hashpw")
+        cls.hashpw_patcher = mock.patch(
+            "app.infra.cryptography.bcrypt_adapter.bcrypt.hashpw"
+        )
+        cls.gensalt_patcher = mock.patch(
+            "app.infra.cryptography.bcrypt_adapter.bcrypt.gensalt"
+        )
 
     def setUp(self) -> None:
         self.hashpw_stub = self.hashpw_patcher.start()
-        self.hashpw_stub.return_value = "hashed_value"
+        self.hashpw_stub.return_value = "hash".encode("utf-8")
+
+        self.gensalt_stub = self.gensalt_patcher.start()
+        self.gensalt_stub.return_value = "salt".encode("utf-8")
 
     def tearDown(self) -> None:
         self.hashpw_patcher.stop()
+        self.gensalt_patcher.stop()
 
-    def test_call_bcrypt_with_correct_values(self):
+    def test_call_gensalt_with_correct_value(self):
         """
         Should call bcrypt with correct values
         """
-        self.hashpw_patcher.stop()
-
-        with mock.patch(
-            "app.infra.cryptography.bcrypt_adapter.bcrypt.hashpw",
-            return_value=b"hashed_value",
-        ) as hashpw_spy:
-            rounds = 12
-            sut = BcryptAdapter(rounds)
-            value = "any_value"
-            sut.encrypt(value)
-            hashpw_spy.assert_called_once_with(value.encode("utf-8"), mock.ANY)
+        rounds = 12
+        sut = BcryptAdapter(rounds)
+        value = "any_value"
+        sut.encrypt(value)
+        self.gensalt_stub.assert_called_once_with(rounds)
 
     def test_call_hashpw_with_correct_values(self):
         """
@@ -46,4 +49,16 @@ class BcryptAdapterTests(TestCase):
         sut = BcryptAdapter(rounds)
         value = "any_value"
         sut.encrypt(value)
-        self.hashpw_stub.assert_called_once_with(value, rounds)
+        self.hashpw_stub.assert_called_once_with(
+            value.encode("utf-8"), "salt".encode("utf-8")
+        )
+
+    def test_returns_hash_on_success(self):
+        """
+        Should returns a hash on success
+        """
+        rounds = 12
+        sut = BcryptAdapter(rounds)
+        value = "any_value"
+        hashed_value = sut.encrypt(value)
+        self.assertEqual(hashed_value, "hash")
