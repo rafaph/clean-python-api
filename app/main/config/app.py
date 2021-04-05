e8b9695bb893e4232ac1ecec01db8bc0fe04697f
Raphael Castro <rafaphcastro@gmail.com>
2021-04-04 22:19:27 -0300

    chore: add orjson as default response class

--- app/main/config/app.py
+++ app/main/config/app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from .middlewares import setup_middlewares
 
-app = FastAPI(debug=True)
+app = FastAPI(debug=True, default_response_class=ORJSONResponse)
 
 setup_middlewares(app)
