cf2114f7d336087c57ce1f6ac20c0917bfecf6ba
Raphael Castro <rafaphcastro@gmail.com>
2021-04-05 22:17:00 -0300

    refactor: ensure that Mongo connects and disconnect properly in fast api hooks

--- app/main/server.py
+++ app/main/server.py
@@ -1,5 +1,19 @@
+import os
+
+from app.infra.db.mongodb.helpers.mongo_helper import MongoHelper
 from app.main.config import make_app
 
 app = make_app()
+
+
+@app.on_event("startup")
+def startup():
+    MongoHelper.connect(url=os.environ["MONGO_URL"])
+
+
+@app.on_event("shutdown")
+def shutdown():
+    MongoHelper.disconnect()
+
 
 __all__ = ["app"]
