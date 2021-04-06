fa621087d523bda54a7837ad005f050a9b9106b5
Raphael Castro <rafaphcastro@gmail.com>
2021-04-05 21:54:50 -0300

    refactor: connect to mongodb before integration tests

--- app/main/routes/signup_routes_test.py
+++ app/main/routes/signup_routes_test.py
@@ -2,7 +2,9 @@ from unittest import TestCase
 
 import pytest
 from fastapi.testclient import TestClient
+from pymongo_inmemory import MongoClient
 
+from app.infra.db.mongodb.helpers.mongo_helper import MongoHelper
 from app.main.config import make_app
 
 
@@ -14,7 +16,16 @@ class SignUpRoutesTests(TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
+        MongoHelper.connect(client_class=MongoClient)
         cls.client = TestClient(make_app())
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        MongoHelper.disconnect()
+
+    def setUp(self) -> None:
+        account_collection = MongoHelper.get_collection("accounts")
+        account_collection.delete_many({})
 
     def test_return_an_account_on_success(self):
         """
