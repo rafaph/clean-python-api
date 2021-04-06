5d28e99b9bb0533a448f82fefa8d2911f43e02c2
Raphael Castro <rafaphcastro@gmail.com>
2021-04-05 23:51:46 -0300

    feat: ensure mongodb reconnect if client is down

--- app/infra/db/mongodb/helpers/mongo_helper.py
+++ app/infra/db/mongodb/helpers/mongo_helper.py
@@ -2,27 +2,48 @@ from typing import Any, Dict, Optional
 
 from pymongo import MongoClient
 from pymongo.collection import Collection
+from pymongo.errors import ConnectionFailure
 
 
 class MongoHelper:
-    client: MongoClient
+    client: Optional[MongoClient] = None
     db: str = "clean-python-api"
+    _url: Optional = None
+    _client_class: Optional = None
+    _kwargs = Optional = None
 
     @classmethod
     def connect(
         cls,
         *,
-        url: Optional[str] = None,
-        client_class: Optional = MongoClient,
+        url=None,
+        client_class=MongoClient,
+        **kwargs,
     ) -> None:
-        cls.client = client_class(url)
+        cls._url = url
+        cls._client_class = client_class
+        cls._kwargs = kwargs
+        cls.client = client_class(url, **kwargs)
 
     @classmethod
     def disconnect(cls) -> None:
         cls.client.close()
+        cls.client = None
+
+    @classmethod
+    def is_connected(cls):
+        if cls.client:
+            try:
+                cls.client.admin.command("ismaster")
+                return True
+            except ConnectionFailure:
+                pass
+        return False
 
     @classmethod
     def get_collection(cls, name: str) -> Collection:
+        if not cls.is_connected():
+            cls.connect(url=cls._url, client_class=cls._client_class, **cls._kwargs)
         return cls.client[cls.db][name]
 
     @staticmethod
