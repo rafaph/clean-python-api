6bdb7f2d77b9345e9ec875e35a31e261fab7f7a0
Raphael Castro <rafaphcastro@gmail.com>
2021-04-11 00:02:54 -0300

    feat: ensure LogControllerDecorator calls controller handle with correct value

--- app/main/factories/signup.py
+++ app/main/factories/signup.py
@@ -1,14 +1,18 @@
 from app.data.usecases.add_account.db_add_account import DbAddAccount
 from app.infra.cryptography.bcrypt_adapter import BcryptAdapter
 from app.infra.db.mongodb.account_repository.account import AccountMongoRepository
+from app.main.decorators.log import LogControllerDecorator
 from app.presentation.controllers.signup.controller import SignUpController
+from app.presentation.protocols import Controller
 from app.utils.email_validator_adapter import EmailValidatorAdapter
 
 
-def make_sign_up_controller() -> SignUpController:
+def make_sign_up_controller() -> Controller:
     emil_validator = EmailValidatorAdapter()
     encrypter = BcryptAdapter(rounds=12)
     add_account_repository = AccountMongoRepository()
     add_account = DbAddAccount(encrypter, add_account_repository)
 
-    return SignUpController(emil_validator, add_account)
+    sign_up_controller = SignUpController(emil_validator, add_account)
+
+    return LogControllerDecorator(sign_up_controller)
--- app/presentation/protocols/controller.py
+++ app/presentation/protocols/controller.py
@@ -5,7 +5,7 @@ from .http import HttpRequest, HttpResponse
 
 class Controller(abc.ABC):
     @abc.abstractmethod
-    def handle(self, httpRequest: HttpRequest) -> HttpResponse:
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
         raise NotImplementedError(
             "Every controller must implement a handle."
         )  # pragma: no cover
