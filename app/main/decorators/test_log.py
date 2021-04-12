8f03ad2654fe1b8245ff4ac58cf0da4a5a9496ff
Raphael Castro <rafaphcastro@gmail.com>
2021-04-11 23:40:10 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- app/main/decorators/test_log.py
+++ app/main/decorators/test_log.py
@@ -4,14 +4,10 @@ from unittest import TestCase, mock
 
 import pytest
 
+from app.data.protocols.log_error_repository import LogErrorRepository
 from app.main.decorators.log import LogControllerDecorator
+from app.presentation.helpers.http_helpers import server_error
 from app.presentation.protocols import Controller, HttpRequest, HttpResponse
-
-
-@dataclass
-class SutTypes:
-    controller_stub: Controller
-    sut: LogControllerDecorator
 
 
 def make_controller() -> Controller:
@@ -22,11 +18,31 @@ def make_controller() -> Controller:
     return ControllerStub()
 
 
+def make_log_error_repository() -> LogErrorRepository:
+    class LogErrorRepositoryStub(LogErrorRepository):
+        def log(self, stack: str) -> None:
+            pass
+
+    return LogErrorRepositoryStub()
+
+
+@dataclass
+class SutTypes:
+    controller_stub: Controller
+    sut: LogControllerDecorator
+    log_error_repository_stub: LogErrorRepository
+
+
 def make_sut() -> SutTypes:
     controller_stub = make_controller()
-    sut = LogControllerDecorator(controller_stub)
+    log_error_repository_stub = make_log_error_repository()
+    sut = LogControllerDecorator(controller_stub, log_error_repository_stub)
 
-    return SutTypes(controller_stub=controller_stub, sut=sut)
+    return SutTypes(
+        controller_stub=controller_stub,
+        sut=sut,
+        log_error_repository_stub=log_error_repository_stub,
+    )
 
 
 @pytest.mark.unit
@@ -60,3 +76,25 @@ class LogControllerDecoratorTests(TestCase):
         self.assertEqual(
             http_response, HttpResponse(status=HTTPStatus.NOT_FOUND, body=None)
         )
+
+    @staticmethod
+    def test_call_log_error_repository_with_correct_error_if_server_error():
+        """
+        Should call LogErrorRepository with correct error if controller \
+returns a server error
+        """
+        sut_types = make_sut()
+        sut, controller_stub, log_error_repository_stub = (
+            sut_types.sut,
+            sut_types.controller_stub,
+            sut_types.log_error_repository_stub,
+        )
+        stack = "any_stack"
+
+        http_request = HttpRequest()
+
+        with mock.patch.object(
+            controller_stub, "handle", return_value=server_error(stack)
+        ), mock.patch.object(log_error_repository_stub, "log") as log_spy:
+            sut.handle(http_request)
+            log_spy.assert_called_once_with(stack)
