a97c26aac48ce4432b98fa91948b3f13b6c41628
Raphael Castro <rafaphcastro@gmail.com>
2021-04-24 19:59:57 -0300

    feat: add LogErrorMongoRepository to SignUpRepository

--- app/main/factories/signup.py
+++ app/main/factories/signup.py
@@ -1,6 +1,7 @@
 from app.data.usecases.add_account.db_add_account import DbAddAccount
 from app.infra.cryptography.bcrypt_adapter import BcryptAdapter
 from app.infra.db.mongodb.account_repository.account import AccountMongoRepository
+from app.infra.db.mongodb.log_repository.log import LogErrorMongoRepository
 from app.main.decorators.log import LogControllerDecorator
 from app.presentation.controllers.signup.controller import SignUpController
 from app.presentation.protocols import Controller
@@ -14,5 +15,6 @@ def make_sign_up_controller() -> Controller:
     add_account = DbAddAccount(encrypter, add_account_repository)
 
     sign_up_controller = SignUpController(emil_validator, add_account)
+    log_error_repository = LogErrorMongoRepository()
 
-    return LogControllerDecorator(sign_up_controller)
+    return LogControllerDecorator(sign_up_controller, log_error_repository)
